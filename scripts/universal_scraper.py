5a84d966f8da41e15eab132d3f1c59d9424775c0
NgigiN <ngigi.nyongo@gmail.com>
2026-05-28 22:23:41 +0300

    feat: introduced clean script to access all scrapers

---
+++ scripts/universal_scraper.py
@@ -0,0 +1,80 @@
+import argparse
+import json
+import os
+import sys
+from pathlib import Path
+
+# Add src to sys.path to allow importing scrapers.
+src_path = str(Path(__file__).parent.parent / "src")
+if src_path not in sys.path:
+    sys.path.insert(0, src_path)
+
+try:
+    from timetable_scrapers import ScraperRegistry
+except ImportError as e:
+    print(f"Could not import scrapers. {e}")
+    sys.exit(1)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Extract University Exam Schedules using a scraper."
+    )
+
+    parser.add_argument("scraper_name", help="Name of the scraper to use")
+
+    parser.add_argument("input_file", help="Path to the Excel file")
+
+    parser.add_argument(
+        "output_file",
+        help="Path to where you want your JSON file to be located and its name",
+    )
+
+    args = parser.parse_args()
+
+    scraper_name = args.scraper_name
+    input_file = args.input_file
+    output_file = args.output_file
+
+    # Ensure the output der exists
+    output_dir = os.path.dirname(output_file)
+    if output_dir:
+        os.makedirs(output_dir, exist_ok=True)
+
+    # Ensure input file exists
+    if not os.path.exists(input_file):
+        print(f"Error: {input_file} does not  exist")
+        sys.exit(1)
+
+    # Load relevant Scraper
+    try:
+        scraper = ScraperRegistry.get_scraper(scraper_name)
+    except ValueError as e:
+        print(f"{e}")
+
+        available = ScraperRegistry.list_scrapers()
+        print(f"Available Scrapers: {available}")
+
+        sys.exit(1)
+
+    try:
+        with open(input_file, "rb") as f:
+            entries = scraper.extract(f)
+    except Exception as e:
+        print(f"Error during extraction: {e}")
+        sys.exit(1)
+
+    print(f"Extracted {len(entries)}")
+
+    items = [entry.to_dict() for entry in entries]
+
+    print(f"Saving to: {output_file}")
+
+    with open(output_file, "w", encoding="utf-8") as f:
+        json.dump(items, f, indent=4, ensure_ascii=False)
+
+    print("Success")
+
+
+if __name__ == "__main__":
+    main()
